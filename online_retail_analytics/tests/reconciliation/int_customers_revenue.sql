9949adc1efc8849f863056ff05287ae4b6dc1f5f
Min Khant Maung Maung <minkhantmgmg.mk19@gmail.com>
2026-07-10 12:57:06 +0700

    chore: add pre-commit hooks and clean whitespace

--- online_retail_analytics/tests/reconciliation/int_customers_revenue.sql
+++ online_retail_analytics/tests/reconciliation/int_customers_revenue.sql
@@ -1,25 +1,25 @@
 
-WITH int_revenue_by_customer AS 
+WITH int_revenue_by_customer AS
 (
- SELECT 
+ SELECT
  customer_id, gross_revenue - cancelled_revenue as total_revenue -- calculate total revenue by subtracting cancelled revenue from gross revenue
 FROM {{ ref('int_customers') }}
 ),
-stg_revenue_by_customer as 
+stg_revenue_by_customer as
 (
-SELECT 
+SELECT
 customer_id, sum(quantity * unit_price) as total_revenue
 FROM {{ ref('stg_invoices') }}
 where customer_id is not null -- ignore null customer_id values
 group by customer_id
 )
-SELECT  
+SELECT
 coalesce(int.customer_id, stg.customer_id) as customer_id,
 int.total_revenue as int_total_revenue,
 stg.total_revenue as stg_total_revenue
 
 FROM int_revenue_by_customer int
-full outer join 
- stg_revenue_by_customer stg 
-on int.customer_id = stg.customer_id 
+full outer join
+ stg_revenue_by_customer stg
+on int.customer_id = stg.customer_id
 where int_total_revenue <> stg_total_revenue
--- online_retail_analytics/tests/reconciliation/int_invoices_revenue.sql
+++ online_retail_analytics/tests/reconciliation/int_invoices_revenue.sql
@@ -1,24 +1,24 @@
 
-WITH int_revenue_by_invoice AS 
+WITH int_revenue_by_invoice AS
 (
- SELECT 
+ SELECT
 invoice_id, total_revenue
 FROM {{ ref('int_invoices') }}
 ),
-stg_revenue_by_invoice as 
+stg_revenue_by_invoice as
 (
-SELECT 
+SELECT
 invoice_id, sum(quantity * unit_price) as total_revenue
 FROM {{ ref('stg_invoices') }}
 group by invoice_id
 )
-SELECT  
+SELECT
 coalesce(int.invoice_id, stg.invoice_id) as invoice_id,
 int.total_revenue as int_total_revenue,
 stg.total_revenue as stg_total_revenue
 
 FROM int_revenue_by_invoice int
-full outer join 
- stg_revenue_by_invoice stg 
-on int.invoice_id = stg.invoice_id 
+full outer join
+ stg_revenue_by_invoice stg
+on int.invoice_id = stg.invoice_id
 where int_total_revenue <> stg_total_revenue
